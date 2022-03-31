1df501bbff3e871796a7c306df7051472ba8300e
diogoCostaProjects <diogocosta.js@gmail.com>
2022-03-31 18:08:15 -0300

    Iniciando a plataforma

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
+
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class,'sobrenos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
