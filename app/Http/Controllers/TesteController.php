92c77cd2ee33a065a726049b52a42ff81fb25ed0
diogoCostaProjects <diogocosta.js@gmail.com>
2022-04-04 19:00:29 -0300

    Passando parâmetros para VIEW

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -6,7 +6,14 @@ use Illuminate\Http\Request;
 
 class TesteController extends Controller
 {
-    public function teste(int $p1, int $p2) {  // recebendo parametros nos controllers
-        echo "A soma de $p1 + $p2 é :".($p1 + $p2);
+    public function teste(int $p1, int $p2) {  // recebendo parametros nos controller
+
+        // $data = ['x'=>$p1, 'y'=>$p2];
+        
+        // return view('site.teste', $data); // array associativo 
+
+        // return view('site.teste', compact('p1', 'p2')); // método compact do php
+        
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2); // método with do laravel
     }
 }
---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,3 @@
+<h3>Teste (View)</h3>
+<!-- DOUBLE MUSTACHE SEMELHANTE AOS FRAMES JAVASCRIPT -->
+{{ $p1 + $p2 }}
