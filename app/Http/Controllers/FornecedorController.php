292b5349b758d52926f6f92965d6a2229a128b2a
diogoCostaProjects <diogocosta.js@gmail.com>
2022-04-05 16:28:22 -0300

    Tratando condicionais e listagem com blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,24 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+
+        $fornecedores = [
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo',
+            'Diogo'
+        ];
+        $data = ['fornecedores'=> $fornecedores];
+
+        return view('app.fornecedor.index', $data);
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,23 @@
 <h3>Fornecedor</h3>
 
-{{-- comentário --}}
 
-@php 
-// comentário dentro do php puro
-@endphp
+
+{{-- @php 
+
+    @if ()
+        
+    @endif -
+
+@endphp --}}
+
+{{-- imprime o array  e a estrutura completa como o Dump  --}}
+{{-- @dd($fornecedores)  --}}
+
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Tem fornecedores</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Tem vários fornecedores</h3>
+@else
+    <h3>ainda não há fornecedores</h3>
+@endif
